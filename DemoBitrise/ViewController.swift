c2a55eb9f7692d87cd21245b57e4912c09408485
Duy Nguyen <nguyenvunhatduy@gmail.com>
2019-11-26 16:08:26 +0800

    init

---
+++ DemoBitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoBitrise
+//
+//  Created by Duy Nguyen on 26/11/19.
+//  Copyright © 2019 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
